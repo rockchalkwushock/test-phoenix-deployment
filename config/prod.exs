207cf0ba7e92881db8d171318dab3055beab3d41
Cody Brunner <rockchalkwushock@icloud.com>
2018-06-22 12:03:50 -0700

    fixes incorrect app names from copying.

--- config/prod.exs
+++ config/prod.exs
@@ -24,7 +24,7 @@ config :my_application, MyApplicationWeb.Endpoint,
 config :logger, level: :info
 
 # Configure your database
-config :hello, Hello.Repo,
+config :my_application, MyApplication.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
